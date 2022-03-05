91ab36c8dfff2f1eb67a398f30762802d98067c3
AADIVENGAT <88031195+AADIVENGAT@users.noreply.github.com>
2022-03-05 07:03:03 +0530

    Add files via upload

---
+++ march3.11.py
@@ -0,0 +1,13 @@
+#Multiple choice questions
+
+print("1.What is 1+1?")
+print("a) 0")
+print("b) 1")
+print("c) 2")
+print("d) 3")       
+tuple=("a","b","c","d")
+x = input("Enter value: ")
+if(x==("c")):
+    print("True") 
+else:  
+    print("None") 
